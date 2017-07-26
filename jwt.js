f71dc61431b1fe1028b0a7383f7199e980586f5b
ibigrock-ashish <ashishgupta@ibigrock.com>
2017-07-27 04:26:36 +0530

    Authorisation

--- jwt.js
+++ jwt.js
@@ -1,14 +1,30 @@
 var query = window.location.search.substring(1);
 //document.write(query);
-var jwtToken = query.split('Authenticate:')[1];
-//document.write(jwtToken);
+var jwtToken = query.split('Authorization:')[1];
+//document.write(jwtToken);Authorization
 try{
 var obj=parseJwt(jwtToken);
-document.write("Tokken Value" + JSON.stringify(obj));
+var hasMatch =false;
+
+for(var admin in obj){
+if(obj.hasOwnProperty(admin))
+	if(obj.admin===true){
+   		hasMatch = true;
+   		break;
+ 	}
+}
+
+if(hasMatch===true){
+	document.write("PASS");
+}else{
+	document.write("Invalid Token");
+	}
 }
 catch(e){
-document.write("incorrect token");
+	document.write("incorrect token");
 	}
+
+
 function parseJwt(jwtToken) {
     return JSON.parse(window.atob(jwtToken.split('.')[1])); //parsing base64Url
 };
